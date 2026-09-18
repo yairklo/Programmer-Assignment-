4f8ed1c11956317a314c6a452616d6b0310a1381
yairklo <yair.klausner@mail.huji.ac.il>
2026-09-18 10:14:00 +0300

    Add css

--- part 1/part-2-save-the-lab/frontend/src/chat/main.tsx
+++ part 1/part-2-save-the-lab/frontend/src/chat/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ChatApp } from "./components/ChatApp";
+import "./chat.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
